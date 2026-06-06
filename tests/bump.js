a01ce1aa450bb4a9975e38b63329967bf5c7a1f0
hudsonjhonson9-arch <hudsonjhonson9@gmail.com>
2026-06-06 22:05:34 +0800

    Delete tests directory

--- tests/bump.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('../index.html', 'utf8');
-html = html.replace(/\.js(\?v=[0-9a-zA-Z]+)?\"/g, '.js?v=' + Date.now() + '"');
-fs.writeFileSync('../index.html', html);
